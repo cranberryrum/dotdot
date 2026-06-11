46bde186f2755494a34ce0eac6f4f90a8f522c26
Aditya <adityakoltedes@gmail.com>
2026-06-11 13:35:32 +0530

    Initial Commit

---
+++ Dot Grid/Dot_GridApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dot_GridApp.swift
+//  Dot Grid
+//
+//  Created by Aditya on 11/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Dot_GridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Dot GridTests/Dot_GridTests.swift
@@ -0,0 +1,17 @@
+//
+//  Dot_GridTests.swift
+//  Dot GridTests
+//
+//  Created by Aditya on 11/06/26.
+//
+
+import Testing
+@testable import Dot_Grid
+
+struct Dot_GridTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
